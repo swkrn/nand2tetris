b0ffbc10afff220db5c20b63db4fa5f05ea42ccf
Your Name <you@example.com>
2025-06-03 21:04:55 +0700

    finished 07: vm translator - stack arithmetic

--- 7/MemoryAccess/BasicTest/BasicTest.asm
+++ 7/MemoryAccess/BasicTest/BasicTest.asm
@@ -1,274 +1,208 @@
-                @10
+@10
 D=A
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
-                @LCL
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@LCL
+D=M
+@0
+D=D+A
+@label.temp.addr
+M=D
+@SP
+AM=M-1
+D=M
+@label.temp.addr
+A=M
+M=D
+@21
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@22
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@ARG
+D=M
+@2
+D=D+A
+@label.temp.addr
+M=D
+@SP
+AM=M-1
+D=M
+@label.temp.addr
+A=M
+M=D
+@ARG
+D=M
+@1
+D=D+A
+@label.temp.addr
+M=D
+@SP
+AM=M-1
+D=M
+@label.temp.addr
+A=M
+M=D
+@36
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THIS
+D=M
+@6
+D=D+A
+@label.temp.addr
+M=D
+@SP
+AM=M-1
+D=M
+@label.temp.addr
+A=M
+M=D
+@42
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@45
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THAT
+D=M
+@5
+D=D+A
+@label.temp.addr
+M=D
+@SP
+AM=M-1
+D=M
+@label.temp.addr
+A=M
+M=D
+@THAT
+D=M
+@2
+D=D+A
+@label.temp.addr
+M=D
+@SP
+AM=M-1
+D=M
+@label.temp.addr
+A=M
+M=D
+@510
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+AM=M-1
+D=M
+@11
+M=D
+@LCL
 D=M
 @0
 A=D+A
 D=M
-
-                // keep target memory in temp (R5)
-                @5
-                M=D
-                @SP
-                M=M-1
-                A=M
-                D=M
-                @5
-                A=M
-                M=D
-                @21
-D=A
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
-                @22
-D=A
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
-                @ARG
-D=M
-@2
-A=D+A
-D=M
-
-                // keep target memory in temp (R5)
-                @5
-                M=D
-                @SP
-                M=M-1
-                A=M
-                D=M
-                @5
-                A=M
-                M=D
-                @ARG
-D=M
-@1
-A=D+A
-D=M
-
-                // keep target memory in temp (R5)
-                @5
-                M=D
-                @SP
-                M=M-1
-                A=M
-                D=M
-                @5
-                A=M
-                M=D
-                @36
-D=A
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
-                @THIS
-D=M
-@6
-A=D+A
-D=M
-
-                // keep target memory in temp (R5)
-                @5
-                M=D
-                @SP
-                M=M-1
-                A=M
-                D=M
-                @5
-                A=M
-                M=D
-                @42
-D=A
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
-                @45
-D=A
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
-                @THAT
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THAT
 D=M
 @5
 A=D+A
 D=M
-
-                // keep target memory in temp (R5)
-                @5
-                M=D
-                @SP
-                M=M-1
-                A=M
-                D=M
-                @5
-                A=M
-                M=D
-                @THAT
-D=M
-@2
-A=D+A
-D=M
-
-                // keep target memory in temp (R5)
-                @5
-                M=D
-                @SP
-                M=M-1
-                A=M
-                D=M
-                @5
-                A=M
-                M=D
-                @510
-D=A
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
-                @5
-D=M
-@6
-A=D+A
-D=M
-
-                // keep target memory in temp (R5)
-                @5
-                M=D
-                @SP
-                M=M-1
-                A=M
-                D=M
-                @5
-                A=M
-                M=D
-                @LCL
-D=M
-@0
-A=D+A
-D=M
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
-                @THAT
-D=M
-@5
-A=D+A
-D=M
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
 @SP
 A=M
-A=A-1
+M=D
+@SP
+M=M+1
+@SP
+AM=M-1
 D=M
 A=A-1
-M=M+D
-@SP
-M=M-1
-                @ARG
+M=D+M
+@ARG
 D=M
 @1
 A=D+A
 D=M
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
 @SP
 A=M
-A=A-1
+M=D
+@SP
+M=M+1
+@SP
+AM=M-1
 D=M
 A=A-1
 M=M-D
-@SP
-M=M-1
-                @THIS
+@THIS
 D=M
 @6
 A=D+A
 D=M
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
-                @THIS
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THIS
 D=M
 @6
 A=D+A
 D=M
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
 @SP
 A=M
-A=A-1
+M=D
+@SP
+M=M+1
+@SP
+AM=M-1
 D=M
 A=A-1
-M=M+D
+M=D+M
 @SP
-M=M-1
-@SP
-A=M
-A=A-1
+AM=M-1
 D=M
 A=A-1
 M=M-D
-@SP
-M=M-1
-                @5
+@11
 D=M
-@6
-A=D+A
-D=M
-
-                @SP
-                A=M
-                M=D
-                @SP
-                M=M+1
 @SP
 A=M
-A=A-1
+M=D
+@SP
+M=M+1
+@SP
+AM=M-1
 D=M
 A=A-1
-M=M+D
-@SP
-M=M-1
+M=D+M
